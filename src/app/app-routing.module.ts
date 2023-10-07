31f86d2fefb1783ec0be776f01b64bc8b1d901cf
Meriem1551 <meriem200409@gmail.com>
2023-10-07 21:54:46 +0200

    Creat Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { AnimesComponent } from './animes/animes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'movies', component: MoviesComponent},
+  {path: 'animes', component: AnimesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
