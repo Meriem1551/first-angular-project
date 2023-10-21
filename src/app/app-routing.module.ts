e28e940e0dbe4c49b4d7443daf8e5e2effe672d9
Meriem1551 <meriem200409@gmail.com>
2023-10-21 16:33:14 +0200

    Importing HttpClientModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
 import { AnimesComponent } from './animes/animes.component';
-
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'movies', component: MoviesComponent},
