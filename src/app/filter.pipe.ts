918780b3de2cc7900c6e344e4f538ed474b7a8fd
Meriem1551 <meriem200409@gmail.com>
2023-10-28 23:17:38 +0200

    Creating movies page

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(movies: any[], term: string): any {
+
+    //check if search term is undefined
+    if (term === undefined) return movies;
+    //return updated ninjas array
+    return movies.filter(function(movies){
+      return movies.name.toLowerCase().includes(term.toLowerCase())
+    });
+  }
+
+}
